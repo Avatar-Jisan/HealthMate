84b347be871071b3d8c6ccd3bba36fbaea02e26a
Avatar-Jisan <abdullaaljisan@gmail.com>
2025-07-04 07:14:05 +0600

    Added Working Calorie Needs Button

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const bmiResult = document.getElementById("bmi-result");
 const bmiBtn = document.getElementById("bmi-btn");
 const bmi = document.getElementById("bmi");
+const calorie = document.getElementById("calorie");
+const calBtn = document.getElementById("cal-btn");
+
 
 bmiBtn.addEventListener("click", function () {
   bmiResult.textContent = "Result: ";
   const weightIn = document.getElementById("weight-in");
   const weightInput = weightIn.querySelector(".weight-kg-in");
-  const bmiCategory = bmi.querySelector("#bmi-category")
- const heightM = bmi.querySelector(".height-m")
+  const bmiCategory = bmi.querySelector("#bmi-category");
+  const heightM = bmi.querySelector(".height-m");
   let weight;
   const bmiWeight = bmi.querySelector(".weight-unit");
   if (bmiWeight.value === "kg") {
@@ -15,49 +18,82 @@ bmiBtn.addEventListener("click", function () {
   } else {
     weight = parseFloat(weightInput.value * 0.453592);
   }
-  const bmiHeightUnit = bmi.querySelector(".height-unit");
-  const bmiHeightCmInput = bmi.querySelector(".height-cm");
-  const bmiHeightFtInput = bmi.querySelector(".height-ft");
-  const bmiHeightInchInput = bmi.querySelector(".height-in");
+  const HeightUnit = bmi.querySelector(".height-unit");
+  const HeightCmInput = bmi.querySelector(".height-cm");
+  const HeightFtInput = bmi.querySelector(".height-ft");
+  const HeightInchInput = bmi.querySelector(".height-in");
   let height;
-  if (bmiHeightUnit.value === "cm") {
-    height = parseFloat(bmiHeightCmInput.value / 100);
+  if (HeightUnit.value === "cm") {
+    height = parseFloat(HeightCmInput.value / 100);
   } else {
-    height = parseFloat(bmiHeightFtInput.value * 0.3048);
-    height += parseFloat(bmiHeightInchInput.value * 0.0254);
+    height = parseFloat(HeightFtInput.value * 0.3048);
+    height += parseFloat(HeightInchInput.value * 0.0254);
   }
   let ogResult = weight / (height * height);
-  let result = ogResult.toFixed(2)
-  heightM.textContent = "Height in Meter: " + height.toFixed(2)
-  bmiResult.textContent += result
-  if (result>= 40.00){
-    bmiCategory.textContent = "Class III obesity"
+  let result = ogResult.toFixed(2);
+  console.log(result);
+  heightM.textContent = "Height in Meter: " + height.toFixed(2);
+  bmiResult.textContent += result;
+  if (result >= 40.0) {
+    bmiCategory.textContent = "Class III obesity";
     bmiCategory.style.backgroundColor = "#8B0000";
-  }
-  else if (result>=35.00 && result<40.00){
-    bmiCategory.textContent= "Class II obesity"
-    bmiCategory.style.backgroundColor = "#DC143C"
-  }
-  else if (result >=30.00 && result< 35.00){
-    bmiCategory.textContent= "Class I obesity"
-    bmiCategory.style.backgroundColor = "#FF4500"
-  }
-  else if (result>=25.00 && result<30.00){
-    bmiCategory.textContent="Overweight"
-    bmiCategory.style.backgroundColor = "#FFD700"
-  }
-  else if (result>=18.50 && result<25.00){
-    bmiCategory.textContent ="Normal weight"
-    bmiCategory.style.backgroundColor = "#4CAF50"
-  }
-  else {
-    bmiCategory.textContent= "Underweight"
-    bmiCategory.style.backgroundColor = "#87CEFA"
+  } else if (result >= 35.0 && result < 40.0) {
+    bmiCategory.textContent = "Class II obesity";
+    bmiCategory.style.backgroundColor = "#DC143C";
+  } else if (result >= 30.0 && result < 35.0) {
+    bmiCategory.textContent = "Class I obesity";
+    bmiCategory.style.backgroundColor = "#FF4500";
+  } else if (result >= 25.0 && result < 30.0) {
+    bmiCategory.textContent = "Overweight";
+    bmiCategory.style.backgroundColor = "#FFD700";
+  } else if (result >= 18.5 && result < 25.0) {
+    bmiCategory.textContent = "Normal weight";
+    bmiCategory.style.backgroundColor = "#4CAF50";
+  } else if (result === "NaN") {
+    bmiCategory.textContent = "";
+  } else {
+    bmiCategory.textContent = "Underweight";
+    bmiCategory.style.backgroundColor = "#87CEFA";
   }
   weightInput.value = "";
-  bmiHeightCmInput.value = "";
-  bmiHeightFtInput.value =""
-  bmiHeightInchInput.value=""
+  HeightCmInput.value = "";
+  HeightFtInput.value = "";
+  HeightInchInput.value = "";
+});
+
+calBtn.addEventListener("click", function () {
+  let weight, height;
+  const weightUnit = calorie.querySelector(".weight-unit");
+  const weightInput = calorie.querySelector(".weight-kg-in");
+  const HeightFtInput = calorie.querySelector(".height-ft");
+  const HeightInchInput = calorie.querySelector(".height-in");
+  if (weightUnit.value === "kg") {
+    weight = parseFloat(weightInput.value);
+  } else {
+    weight = parseFloat(weightInput.value * 0.453592);
+  }
+  const HeightUnit = calorie.querySelector(".height-unit");
+  const HeightCm = calorie.querySelector(".height-cm");
+  if (HeightUnit.value === "cm") {
+    height = parseFloat(HeightCm.value / 100);
+  } else {
+    height = parseFloat(HeightFtInput.value * 0.3048);
+    height += parseFloat(HeightInchInput.value * 0.0254);
+  }
+  const age = document.getElementById("cal-age").value
+  const gender = document.getElementById("cal-gender")
+  let BMR,result,ogResult;
+  if(gender.value==="male"){
+   BMR = 10 * weight + 625 * height - 5 * age + 5
+  }
+  else if ( gender.value === "female"){
+    BMR = 10 * weight + 625 * height(m) - 5 * age - 161
+  }
+  const activity = document.getElementById("cal-activity").value
+  ogResult = BMR * activity 
+  result = ogResult.toFixed(2)
+  const calorieREsult = document.getElementById("cal-result")
+  calorieREsult.textContent = "Daily Calorie Needs: "+ result + "kcal"
 });
 
 document.querySelectorAll(".height-unit").forEach((select) => {
